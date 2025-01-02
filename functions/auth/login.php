c90bbee67fb24e3ec661bc45bdb3e4ba32beadfe
Max <mmtito@yavirac.edu.ec>
2025-01-01 20:11:34 -0500

    V.2.2 Add SweetAlert

--- functions/auth/login.php
+++ functions/auth/login.php
@@ -1,21 +1,43 @@
 <?php
 session_start();
+include('../../shared/header.php');
+
 $name = 'max';
 $password = 'max';
 $namebtn = $_POST['name'];
 $passwordbtn = $_POST['password'];
 
-if ($name === $namebtn && $password === $passwordbtn) {
+if ($name === $namebtn && $password === $passwordbtn) { 
     $_SESSION['usuario'] = $name;
-    header('Location: /onlineShop/views/auth/logged.php');
-    exit();
+    ?>
+    <script>
+        window.onload = function() {
+            Swal.fire({
+                position: "top-center",
+                icon: "success",
+                title: "Has iniciado session correctamente",
+                showConfirmButton: false,
+                timer: 1000
+            }).then(() => {
+                window.location = "/onlineShop/views/auth/logged.php"; // Redirigir después de la alerta
+            });
+        };
+    </script>
+    <?php
 } else { ?>
-<script>
-    alert('usuario o contraseña no coincide con los registrados');
-    window.location = "/onlineShop"
-</script>
+    <script>
+        // Esperar que el DOM esté completamente cargado antes de ejecutar el script
+        window.onload = function() {
+            Swal.fire({
+                icon: 'error',
+                title: 'Error',
+                text: 'Usuario o contraseña no coincide con los registrados',
+                confirmButtonText: 'Regresar'
+            }).then(() => {
+                window.location = "/onlineShop"; // Redirigir después de la alerta
+            });
+        };
+    </script>
 <?php
-    exit();
 }
-
 ?>
